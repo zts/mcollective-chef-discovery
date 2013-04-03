0f881d2004523a5f1026baf34367790f894f1472
Zachary Stevens <zts@cryptocracy.com>
2013-04-03 14:25:41 +0100

    Load Chef server details from Knife configuration.

--- discovery/chef.rb
+++ discovery/chef.rb
@@ -1,19 +1,14 @@
 # discovers against a Chef server
 
+require 'chef/knife'
+require 'chef/config'
 require 'spice'
 
 module MCollective
   class Discovery
     class Chef
       def self.discover(filter, timeout, limit=0, client=nil)
-        cfg = Config.instance
-
-        keyfile                  = cfg.pluginconf["chef-discovery.client_key"]
-        Spice.client_key         = Spice.read_key_file(keyfile)
-        Spice.client_name        = cfg.pluginconf["chef-discovery.client_name"]
-
-        Spice.server_url         = cfg.pluginconf["chef-discovery.server_url"]
-        Spice.connection_options = { :ssl => {:verify => false} }
+        load_chef_configuration
 
         search_terms = client.options[:discovery_options]
 
@@ -44,6 +39,41 @@ module MCollective
         /(?<type>(role|recipe))\[(?<name>[^\]]*)\]/ =~ c
         name.gsub!(/:/, '\:')
         return "#{type}:#{name}"
+      end
+
+      def self.load_chef_configuration
+        Spice.reset
+        ::Chef::Knife.new.configure_chef
+        chef = ::Chef::Config
+
+        # Spice copes poorly when chef_server_url has a trailing slash.
+        chef_server_url = chef.chef_server_url.gsub!(/\/$/, '')
+
+        # Choose the correct SSL verification mode
+        if chef.ssl_verify_mode == :verify_none
+          ssl_verify_mode = OpenSSL::SSL::VERIFY_NONE
+        else
+          ssl_verify_mode = OpenSSL::SSL::VERIFY_PEER
+        end
+
+        # Load the Chef private key from disk
+        client_key = Spice.read_key_file(File.expand_path(chef.client_key))
+
+        # Configure Spice
+        Spice.setup do |s|
+          s.server_url  = chef_server_url
+          s.client_name = chef.node_name
+          s.client_key  = client_key
+          s.connection_options = {
+            :ssl => {
+              :verify_mode => ssl_verify_mode,
+              :client_cert => chef.ssl_client_cert,
+              :client_key  => chef.ssl_client_key,
+              :ca_path     => chef.ssl_ca_path,
+              :ca_file     => chef.ssl_ca_file,
+            }
+          }
+        end
       end
     end
   end
